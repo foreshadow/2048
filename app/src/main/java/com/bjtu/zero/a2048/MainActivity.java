b0dfd7a63612d2161f0d717171d117db9bba887e
862083951 <862083951@qq.com>
2016-07-21 01:14:53 +0800

    hah

--- app/src/main/java/com/bjtu/zero/a2048/MainActivity.java
+++ app/src/main/java/com/bjtu/zero/a2048/MainActivity.java
@@ -2,7 +2,7 @@ package com.bjtu.zero.a2048;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,17 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> test=new ArrayList<>() ;
+        ArrayAdapter<String> adapter =new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,test);
     }
 
-    public void update(ArrayList<Block> blocks) {
+    public void update(ArrayList<Block> blocks) { //直接刷新
+        // TODO: 2016/7/21
+    }
+
+    public void spawn(Block block) {//新生一个block的动画
         // TODO: 2016/7/21  
     }
 
-    public void spawn(Block block) {
-        // TODO: 2016/7/21  
-    }
-
-    public void translation(BlockChangeList list) {
+    public void translation(BlockChangeList list) { //平移动画
         // TODO: 2016/7/21     
     }
 
