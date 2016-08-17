4842e724143243174ceb13349f0e1312774aee2d
Infinity <573679956@qq.com>
2016-08-17 15:39:54 +0800

    create thumbnail

--- app/src/main/java/com/bjtu/zero/a2048/ui/GameLayout.java
+++ app/src/main/java/com/bjtu/zero/a2048/ui/GameLayout.java
@@ -100,7 +100,7 @@ public class GameLayout extends FrameLayout {
         }
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                setBlock(i, j, board.getData()[i][j], true);
+                setBlock(i, j, board.getBlock(i, j), true);
             }
         }
     }
